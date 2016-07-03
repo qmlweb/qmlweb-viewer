f8c7a96da0eb5bb4e1e7a5e478955a875794fa13
Сковорода Никита Андреевич <chalkerx@gmail.com>
2016-07-04 00:27:18 +0300

    Inherit stdio to print all server-side logs to console

--- qmlweb-viewer.js
+++ qmlweb-viewer.js
@@ -4,7 +4,7 @@ const electron = require('electron-prebuilt');
 const child_process = require('child_process');
 
 try {
-  child_process.execFileSync(electron, [__dirname].concat(process.argv.slice(2)));
+  child_process.execFileSync(electron, [__dirname].concat(process.argv.slice(2)), { stdio: 'inherit' });
 } catch (e) {
   process.exitCode = e.status;
 }
